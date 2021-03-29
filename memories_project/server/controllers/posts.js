527e31839fbc1d9c5e6bdffc3d35a1db26a6db58
Daniel Terry <daniel-terry@lambdastudents.com>
2021-03-28 21:32:15 -0400

    Added button icons and started building out functionality

--- memories_project/server/controllers/posts.js
+++ memories_project/server/controllers/posts.js
@@ -2,6 +2,7 @@ import PostMessage from '../models/postMessage.js'
 
 //all of the backend actions for post are written out here 
 
+//Fetching Data
 export const getPosts = async (req,res) => {
     //writing try/catch to pull all of the post currently in the database
     try{
@@ -14,6 +15,7 @@ export const getPosts = async (req,res) => {
     }
 }
 
+//Create
 export const createPost = async (req,res) => {
     const post = req.body;
 
@@ -27,4 +29,17 @@ export const createPost = async (req,res) => {
     catch (error) {
         res.status(409).json({ message: error.message });
     }
+}
+
+//Update
+export const updatePost = async (req,res) => {
+    //extract id from the params to target what to update - restructuring it with _id
+    const { id: _id } = req.params;
+    const post = req.body;
+    //check if the id is a mongoose type id
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    //asynchronoous action
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new: true});
+
+    res.json(updatedPost)
 }
--- memories_project/server/routes/posts.js
+++ memories_project/server/routes/posts.js
@@ -1,10 +1,15 @@
 import express from 'express';
 
-import { getPosts,createPost } from '../controllers/posts.js';
+import { getPosts,createPost,updatePost } from '../controllers/posts.js';
 
 const router = express.Router();
 //keeping all of the functionality of our post clean and importing the functionality from controllers
+
+//Fetching data
 router.get('/', getPosts );
+//Create
 router.post('/', createPost );
+//Update
+router.patch('/:id', updatePost)
 
 export default router;
